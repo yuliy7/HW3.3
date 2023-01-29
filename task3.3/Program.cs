edaacee268e473a2aa4f99e8cb6d1a864737fef2
julia B <yuliy-7@yandex.ru>
2023-01-29 14:15:23 +0300

    HW3.3

---
+++ task3.3/Program.cs
@@ -0,0 +1,19 @@
+﻿/*Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+3 -> 1, 8, 27
+5 -> 1, 8, 27, 64, 125*/
+
+int Prompt(string massage)
+{
+    Console.Write($"{massage}: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+int x = Prompt("Введите число N");
+int i = 1;
+while (i<=x)
+{
+    double r = Math.Pow(i, 3);
+    System.Console.WriteLine($"куб числа {i} равен {r}");
+    i++;
+}
+
+
